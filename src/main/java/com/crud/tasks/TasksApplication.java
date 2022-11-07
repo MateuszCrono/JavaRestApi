19bf25296915817802b66e82973cc709f28da770
Mateusz Olszewski <matcrono@gmail.com>
2022-11-07 12:09:38 +0800

    Clear lombok test

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,24 +1,14 @@
 package com.crud.tasks;
 
-import com.crud.tasks.domain.TaskDto;
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TasksApplication {
 
-	public static void main(String[] args) {
-		TaskDto taskDto = new TaskDto(
-				(long)1,
-		"test title",
-		"I want to be a coder");
+    public static void main(String[] args) {
 
-		Long id = taskDto.getId();
-		String title = taskDto.getTitle();
-		String content = taskDto.getContent();
+        SpringApplication.run(TasksApplication.class, args);
+    }
 
-		System.out.println(id + " " + title + " " + content);
-	}
-	// SpringApplication.run(TasksApplication.class, args);
 }
