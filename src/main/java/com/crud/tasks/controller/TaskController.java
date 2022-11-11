6a99a369ffddef17c15c9a90851259e082bf5151
Mateusz Olszewski <matcrono@gmail.com>
2022-11-11 15:03:32 +0800

    19.2 getById

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,19 +1,37 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
 @RestController
 @RequestMapping("/v1/tasks")
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class TaskController {
+
+    private DbService service;
+
+    private TaskMapper taskMapper;
+
+    @Autowired
+    public TaskController(DbService service, TaskMapper taskMapper) {
+        this.service = service;
+        this.taskMapper = taskMapper;
+    }
 
     @GetMapping
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
     }
 
     @GetMapping(value = "{taskId}")
--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -2,11 +2,32 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
-@Getter
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+@NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Entity(name = "tasks")
 public class Task {
+    @javax.persistence.Id
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(name = "name")
     private String title;
+
+    @Column(name = "description")
     private String content;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
 }
