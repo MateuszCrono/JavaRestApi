381d12b890ab8a145cecc769d6f51e068b46914b
Mateusz Olszewski <matcrono@gmail.com>
2023-01-23 14:02:01 +0800

    Task 22.3

--- src/main/java/com/crud/tasks/domain/Trello.java
+++ src/main/java/com/crud/tasks/domain/Trello.java
@@ -1,2 +1,18 @@
-package com.crud.tasks.domain;public class Trello {
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class Trello {
+
+    @JsonProperty("board")
+    public int board;
+
+    @JsonProperty("card")
+    public int card;
+
 }
