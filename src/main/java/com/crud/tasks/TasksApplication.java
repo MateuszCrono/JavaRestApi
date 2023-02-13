b306113134950f8cb67d9aa905fa016bdab0fe23
Mateusz Olszewski <matcrono@gmail.com>
2023-02-13 13:54:40 +0800

    fixing docker ?

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -2,6 +2,7 @@ package com.crud.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 //import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 //
 //@EnableWebMvc
