be9b6b3a8f3b45beb3354684c18a7bc31ff87e3d
Mateusz Olszewski <matcrono@gmail.com>
2023-02-13 13:38:38 +0800

    fighting with mogenius

--- src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -1,50 +1,50 @@
-package com.crud.tasks.service;
-
-import com.crud.tasks.domain.Mail;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
-@ExtendWith(MockitoExtension.class)
-class SimpleEmailServiceTest {
-
-    @InjectMocks
-    private SimpleEmailService simpleEmailService;
-
-    @Mock
-    private JavaMailSender javaMailSender;
-
-//    @Test
-//    public void shouldSendEmail() {
+//package com.crud.tasks.service;
 //
-//        //Given
-//        Mail mail = Mail.builder  ()
-//                .mailTo("test@test.com")
-//                .subject("Test")
-//                .message("Test Message")
-//                .toCc("ccfriend@test.com")
-//                .build();
+//import com.crud.tasks.domain.Mail;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
 //
-//        SimpleMailMessage mailMessage = new SimpleMailMessage();
-//        mailMessage.setTo(mail.getMailTo());
-//        mailMessage.setSubject(mail.getSubject());
-//        mailMessage.setText(mail.getMessage());
-//        mailMessage.setCc(mail.getToCc());
+//import static org.mockito.Mockito.times;
+//import static org.mockito.Mockito.verify;
 //
-//        //When
-//        simpleEmailService.send(mail);
+//@ExtendWith(MockitoExtension.class)
+//class SimpleEmailServiceTest {
 //
-//        //Then
-//        verify(javaMailSender, times(1)).send(mailMessage);
+//    @InjectMocks
+//    private SimpleEmailService simpleEmailService;
 //
+//    @Mock
+//    private JavaMailSender javaMailSender;
 //
-//    }
-
-}
+////    @Test
+////    public void shouldSendEmail() {
+////
+////        //Given
+////        Mail mail = Mail.builder  ()
+////                .mailTo("test@test.com")
+////                .subject("Test")
+////                .message("Test Message")
+////                .toCc("ccfriend@test.com")
+////                .build();
+////
+////        SimpleMailMessage mailMessage = new SimpleMailMessage();
+////        mailMessage.setTo(mail.getMailTo());
+////        mailMessage.setSubject(mail.getSubject());
+////        mailMessage.setText(mail.getMessage());
+////        mailMessage.setCc(mail.getToCc());
+////
+////        //When
+////        simpleEmailService.send(mail);
+////
+////        //Then
+////        verify(javaMailSender, times(1)).send(mailMessage);
+////
+////
+////    }
+//
+//}
