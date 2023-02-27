dddef4b4f5ad71519de0a5d2a9f9882a0a562f0d
Mateusz Olszewski <matcrono@gmail.com>
2023-02-27 17:50:33 +0800

    32.2

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -3,11 +3,9 @@ package com.crud.tasks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//
-//@EnableWebMvc
 @SpringBootApplication
 public class TasksApplication {
+
 
     public static void main(String[] args) {
 
